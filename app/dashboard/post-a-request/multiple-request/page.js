bec9842c9691edc910ab097e57222adac19b2dc0
Codeeq <errytage@gmail.com>
2024-07-22 12:38:32 +0100

    fix linter errros

--- app/dashboard/post-a-request/multiple-request/page.js
+++ app/dashboard/post-a-request/multiple-request/page.js
@@ -1,27 +1,22 @@
 import MultipleRequest from '@/components/DashboardComp/MultipleRequest/MultipleRequest';
+import GoBack from '@/components/GoBack';
 import React from 'react';
 
 export default function page() {
   return (
     <main className='container flex flex-col '>
-      <div>
-        <button
-          onClick={() => nav(-1)}
-          className='viewMore mt-2 flex items-center'
-        >
-          {' '}
-          <BsChevronLeft /> Go Back
-        </button>
-      </div>
+      <header>
+        <GoBack />
+      </header>
 
-      <section className='text-center mb-3 mt-5 w-full md:6 mx-auto'>
+      <article className='text-center mb-3 mt-5 w-full md:6 mx-auto'>
         {' '}
-        <h2 className='font-bold'>Post A Request</h2>
+        <h2 className='font-bold text-2xl'>Post A Request</h2>
         <p className='my-4 text-grey-300 '>
           Make sure you have filled in all the necessary fields and have
           uploaded all the required files.
         </p>{' '}
-      </section>
+      </article>
 
       <section className=' mb-5 w-full md:10 mx-auto'>
         <MultipleRequest />
